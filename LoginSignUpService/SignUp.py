121536312943d1cfd993199883c47c642f3fcf9e
nevinhall <nevinhall98@gmail.com>
2021-02-16 14:34:32 +0000

    Scaffold signup

---
+++ LoginSignUpService/SignUp.py
@@ -0,0 +1,17 @@
+from validate_email import validate_email
+
+
+def signUp(email,password):
+    if(valid_email(email)):
+        """
+        write to database
+        re-direct to login
+        """
+    """
+    re-direct to login
+    """
+
+    pass
+
+def valid_email(email):
+    return validate_email(email)
