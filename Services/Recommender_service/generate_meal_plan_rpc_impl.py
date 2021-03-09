5444f3850a352a1b4bdabd56df8cf3b4f191d0af
nevinhall <nevinhall98@gmail.com>
2021-03-09 15:46:45 +0000

    create ratings from user profile

--- Services/Recommender_service/generate_meal_plan_rpc_impl.py
+++ Services/Recommender_service/generate_meal_plan_rpc_impl.py
@@ -1,10 +1,12 @@
 
+
 import re
 import pika
 import json
 import pymysql
 from validate_email import validate_email
 import uuid
+import io
 
 class rpc_call(object):
 
@@ -102,12 +104,78 @@ React app calls this first to determine what page to render
 def retrieve_user_details(user_id):
     user_profile = rpc_call().get_user_profile(user_id)
     print("This is the user profile", user_profile)
-    return user_profile
+    return normalise_data(user_profile)
 
 
 def normalise_data(response):
     print("reponse in gen meal plan") 
-    print("normaise_data", response)
+    user_profile = response.decode()
+
+    user_profile = user_profile.strip().replace('\'','').replace(',','').replace('(','').replace(')','').split()
+    user_id =  user_profile[0]
+    height = user_profile[1]
+    weight =user_profile[2]
+    bmi =user_profile[3]
+    activity_level =user_profile[4]
+    dietary_options = user_profile[5]
+    allergies = user_profile[6]
+    age = user_profile[7]
+
+    weight_gain = 0
+    weight_lose = 0
+    weight_maintaince = 0
+
+
+    if(float(bmi) < 18.):
+        weight_gain = weight_gain + 3
+    
+    elif(float(bmi) > 25):
+        weight_lose = weight_lose +3
+    else:
+        weight_maintaince = weight_maintaince +3
+
+    
+
+    if(float(bmi) < 18.5 and activity_level == 'high'):
+        weight_gain  = weight_gain + 1
+
+    elif(float(bmi) > 25 and activity_level == 'low'):
+        weight_lose  = weight_lose + 1
+    
+    else:
+        weight_maintaince = weight_maintaince +1
+
+
+    if(float(bmi) < 18.5 and int(age) < 25):
+            weight_gain  = weight_gain + 1
+
+    elif(float(bmi) > 25 and int(age) < 25):
+        weight_lose  = weight_lose + 1
+    
+    else:
+        weight_maintaince = weight_maintaince +1
+
+    print("weight_maintaince", weight_maintaince)
+    print("weight_gain", weight_gain)
+    print("weight_lose", weight_lose)
+
+    
+    print(user_profile)
+
+ 
+  
+
+   # user_profile =user_profile.strip().split("")
+    
+
+   
+   
+   
+    
+
+    
+    # content = line.strip().split("\t")
+    # numerical_data = str(content[1][1:-1]).split(",")
 
     return "retrieving data for", response
 
