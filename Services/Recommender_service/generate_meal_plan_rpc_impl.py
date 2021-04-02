e4d2e408d9b8df821bc3334cea10f366ee4ad669
nevinhall <nevinhall98@gmail.com>
2021-04-02 10:48:55 +0100

    Write meal and exercise plans to db

--- Services/Recommender_service/generate_meal_plan_rpc_impl.py
+++ Services/Recommender_service/generate_meal_plan_rpc_impl.py
@@ -1,15 +1,16 @@
-
 import re
 import pika
 import json
 import pymysql
+from pymongo import MongoClient
 from validate_email import validate_email
 import uuid
 import io
 import pandas as pd
 import rpc_call
-import Create_meal_plan_weights
+from recommender_system import Create_meal_plan_weights
 import prep_data
+from recommender_system import Combinatorial_algorithm
 
 
 """
@@ -38,7 +39,15 @@ def on_request_retrieve_user_details(ch, method, props, body):
     user_profile = retrieve_user_details(body)
     user_profile_normalised = prep_data.normalise_data(user_profile)
     user_profile_weights = Create_meal_plan_weights.Create_meal_plan_weights().create_meal_plan_weights(user_profile_normalised)
-    response = Create_meal_plan_weights.Create_meal_plan_weights().combinatorial_optimisation(user_profile_weights)
+
+    user_profile_weights = user_profile_weights[0]
+    total_calories = 1500
+    is_optimal = False
+
+
+    response = Combinatorial_algorithm.Combinatorial_algorithm().create_meal_plan(is_optimal,user_profile_weights,total_calories)
+    write_meal_plan_to_database(response,body)
+
 
     
     
@@ -64,6 +73,14 @@ def retrieve_user_details(user_id):
     return user_profile
 
 
+
+def write_meal_plan_to_database(mealplan,user_id):
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db =client.meal
+    #db[f"{user_id}"].drop()
+    db[f"{user_id}"].insert_one({"inUse":0,"favourited":0,"mealplan":mealplan})
+
+    print(db[f"{user_id}"].find_one())
 
 
 
