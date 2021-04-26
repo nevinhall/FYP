fbd1a0133a31ad5583cc8adeef0c29bf3118ada1
nevinhall <nevinhall98@gmail.com>
2021-04-26 11:44:41 +0100

    implement get num users gender

--- Services/Admin/admin_rpc_impl.py
+++ Services/Admin/admin_rpc_impl.py
@@ -8,20 +8,27 @@ connection = pika.BlockingConnection(
 channel = connection.channel()
 
 channel.queue_declare(queue='get_num_users_rpc_queue')
+channel.queue_declare(queue='get_num_users_gender_rpc_queue')
 
 
 #database connection
-connection = pymysql.connect(host="localhost",user="root",passwd="",database="users" )
-cursor = connection.cursor()
+connection_user = pymysql.connect(host="localhost",user="root",passwd="",database="users" )
+cursor_user = connection_user.cursor()
+
+connection_user_profiles = pymysql.connect(host="localhost",user="root",passwd="",database="user_profiles" )
+cursor_user_profiles = connection_user_profiles.cursor()
 
 
 
-def get_num_users(): 
+
+
+def get_num_users():
+   
     
     try:
-        sql = "SELECT COUNT(user_id) FROM users"
-        cursor.execute(sql)
-        result = cursor.fetchall()
+        sql = "SELECT COUNT(user_id) FROM user_profiles"
+        cursor_user_profiles.execute(sql)
+        result = cursor_user_profiles.fetchall()
         result = result[0][0]
     except:
         result = ("failure")
@@ -30,10 +37,36 @@ def get_num_users():
     return(result)
 
 
+def get_num_user_gender(gender):
+    
+    try:
+        sql = "SELECT COUNT(user_id) FROM user_profiles WHERE gender =%s"
+        cursor_user_profiles.execute(sql,gender)
+        result = cursor_user_profiles.fetchall()
+        result = result[0][0]
+    except:
+        result = ("failure")
 
 
+    return(result)
+
     
+
+def on_request_get_num_users_gender(ch, method, props, body):
+    body = json.loads(body)
+
+    response = get_num_user_gender(body["gender"])
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
     
+
+
+
 
 def on_request_get_num_users(ch, method, props, body):
  
@@ -52,6 +85,7 @@ def on_request_get_num_users(ch, method, props, body):
 channel.basic_qos(prefetch_count=1)
 
 channel.basic_consume(queue='get_num_users_rpc_queue', on_message_callback=on_request_get_num_users)
+channel.basic_consume(queue='get_num_users_gender_rpc_queue', on_message_callback=on_request_get_num_users_gender)
 
 
 print("Admin service")
